7cc6107749a85f8d36a4aef4f07a9b6b09e56236
Uwe L. Korn <uwe.korn@quantco.com>
2023-03-24 09:53:24 +0100

    Cleanup

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,21 +1,17 @@
 @echo on
 
 set "PYO3_PYTHON=%PYTHON%"
+set "CMAKE_GENERATOR=NMake Makefiles"
 set OPENSSL_NO_VENDOR=1
 set "OPENSSL_DIR=%LIBRARY_PREFIX%"
 
 cp %SRC_DIR%\README.md %SRC_DIR%\connectorx-python\README.md
 cp %SRC_DIR%\LICENSE %SRC_DIR%\connectorx-python\LICENSE
 
-set "CMAKE_GENERATOR=NMake Makefiles"
 cd connectorx-python
 %PYTHON% -m pip install --ignore-installed --no-deps -vv .
-@rem maturin build --no-sdist --release --strip --manylinux off --interpreter=%PYTHON% -m connectorx-python/Cargo.toml
 if errorlevel 1 exit 1
 
-@rem FOR /F "delims=" %%i IN ('dir /s /b target\wheels\*.whl') DO set connectorx_wheel=%%i
-@rem %PYTHON% -m pip install --ignore-installed --no-deps %connectorx_wheel% -vv
-@rem if errorlevel 1 exit 1
-
+cd ..
 cargo-bundle-licenses --format yaml --output THIRDPARTY.yml
 if errorlevel 1 exit 1
