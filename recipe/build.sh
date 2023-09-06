bf436582677624e620ac0495b976a946dcc32e0a
Bela Stoyan <bela.stoyan@gmail.com>
2023-09-06 15:14:08 +0200

    add ""debug"" statements backin

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,8 @@
 set -ex
 
 export PYO3_PYTHON_VERSION=${PY_VER}
+export BINDGEN_EXTRA_CLANG_ARGS="$CFLAGS"
+export LIBCLANG_PATH=$BUILD_PREFIX/lib/libclang${SHLIB_EXT}
 
 maturin build --release --strip --manylinux off --interpreter="${PYTHON}" -m connectorx-python/Cargo.toml
 
