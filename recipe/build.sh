52eb092da5c237dc445e06b365e30785a0ede38e
Bela Stoyan <bela.stoyan@gmail.com>
2023-09-06 14:33:56 +0200

    remove --no-sdist

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ set -ex
 
 export PYO3_PYTHON_VERSION=${PY_VER}
 
-maturin build --no-sdist --release --strip --manylinux off --interpreter="${PYTHON}" -m connectorx-python/Cargo.toml
+maturin build --release --strip --manylinux off --interpreter="${PYTHON}" -m connectorx-python/Cargo.toml
 
 "${PYTHON}" -m pip install $SRC_DIR/connectorx-python/target/wheels/*.whl --no-deps -vv
 
