7d32a8902c139aeed1508e92d504e0f56e95a4bb
Bela Stoyan <bela.stoyan@gmail.com>
2023-09-06 14:10:56 +0200

    remove debug vars

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,12 +3,8 @@
 set -ex
 
 export PYO3_PYTHON_VERSION=${PY_VER}
-export BINDGEN_EXTRA_CLANG_ARGS="$CFLAGS"
-export LIBCLANG_PATH=$BUILD_PREFIX/lib/libclang${SHLIB_EXT}
-export RUST_BACKTRACE=full
-export CARGO_PROFILE_RELEASE_BUILD_OVERRIDE_DEBUG=true
 
-maturin build --release --strip --manylinux off --interpreter="${PYTHON}" -m connectorx-python/Cargo.toml
+maturin build --no-sdist --release --strip --manylinux off --interpreter="${PYTHON}" -m connectorx-python/Cargo.toml
 
 "${PYTHON}" -m pip install $SRC_DIR/connectorx-python/target/wheels/*.whl --no-deps -vv
 
