7a113dbd8b135327336218ec1d6c5ddedb89b413
Uwe L. Korn <uwe.korn@quantco.com>
2025-04-30 13:16:31 +0200

    Add rustflags

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,10 @@ set -ex
 export BINDGEN_EXTRA_CLANG_ARGS="$CFLAGS"
 export LIBCLANG_PATH=$BUILD_PREFIX/lib/libclang${SHLIB_EXT}
 
+if [[ "${target_platform}" == linux-* ]]; then
+  export RUSTFLAGS="-C link-arg=-Wl,-rpath-link,${PREFIX}/lib -C link-arg=-Wl,-rpath,${PREFIX}/lib"
+fi
+
 maturin build --release --strip --manylinux off --interpreter="${PYTHON}" -m connectorx-python/Cargo.toml
 
 "${PYTHON}" -m pip install $SRC_DIR/connectorx-python/target/wheels/*.whl --no-deps -vv
