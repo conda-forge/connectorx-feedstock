f428335df89f3b514536e0af55a0f088ad3341f4
Uwe Korn <uwelk@xhochy.com>
2025-05-01 06:45:18 +0000

    Add -L

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,7 +6,7 @@ export BINDGEN_EXTRA_CLANG_ARGS="$CFLAGS"
 export LIBCLANG_PATH=$BUILD_PREFIX/lib/libclang${SHLIB_EXT}
 
 if [[ "${target_platform}" == linux-* ]]; then
-  export RUSTFLAGS="-C link-arg=-Wl,-rpath-link,${PREFIX}/lib -C link-arg=-Wl,-rpath,${PREFIX}/lib"
+  export RUSTFLAGS="-C link-arg=-Wl,-rpath-link,${PREFIX}/lib -C link-arg=-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib"
 fi
 
 maturin build --release --strip --manylinux off --interpreter="${PYTHON}" -m connectorx-python/Cargo.toml
