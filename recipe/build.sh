9f69511b37ea9ae6a1f142d49c4fb522eaf25f8c
Uwe Korn <uwelk@xhochy.com>
2025-05-04 07:42:50 +0000

    Don't set no-plt

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,6 +9,11 @@ if [[ "${target_platform}" == linux-* ]]; then
   export RUSTFLAGS="-C link-arg=-Wl,-rpath-link,${PREFIX}/lib -C link-arg=-Wl,-rpath,${PREFIX}/lib -L${PREFIX}/lib"
 fi
 
+if [[ "${target_platform}" == "linux-ppc64le" ]]; then
+  export CFLAGS=${CFLAGS//-fno-plt/}
+  export CXXFLAGS=${CXXFLAGS//-fno-plt/}
+fi
+
 maturin build --release --strip --manylinux off --interpreter="${PYTHON}" -m connectorx-python/Cargo.toml
 
 "${PYTHON}" -m pip install $SRC_DIR/connectorx-python/target/wheels/*.whl --no-deps -vv
